23fb3bf6df7fb128e6164e5b55076128ad3a262e
Heavenly <heavenlypaints33@gmail.com.com>
2026-02-05 22:05:28 -0600

    Initial commit (clean)

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import os
 from app import create_app, db
 
 app = create_app()
-
+with app.app_context():
+    db.create_all() 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
