1c3a5ac7c502cd1c9857d2b51dd98f62dcbbc9a8
Heavenly <heavenlypaints33@gmail.com.com>
2026-02-06 16:20:38 -0600

    initial commit(clean)

--- run.py
+++ run.py
@@ -1,10 +1,37 @@
-import os
 from app import create_app, db
 
 app = create_app()
+
 with app.app_context():
-    db.create_all() 
+    db.create_all()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import os
+#from app import create_app, db
+
+#app = create_app()
+#with app.app_context():
+ #   db.create_all() 
+
+#if __name__ == "__main__":
+ #   port = int(os.environ.get("PORT", 5000))
+  #  app.run(host="0.0.0.0", port=port)
