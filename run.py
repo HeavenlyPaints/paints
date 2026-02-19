1bbd4105ef331e01766a95117143a9ea76b38c9b
Heavenly <heavenlypaints33@gmail.com.com>
2026-02-19 05:31:06 -0600

    remove stripe key from code

--- run.py
+++ run.py
@@ -1,4 +1,35 @@
+#from app import create_app, db
+
+#app = create_app()
+
+#with app.app_context():
+ #   db.create_all()
+
+#if __name__ == "__main__":
+ #   app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+from dotenv import load_dotenv
 from app import create_app, db
+
+load_dotenv()
 
 app = create_app()
 
@@ -6,32 +37,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#import os
-#from app import create_app, db
-
-#app = create_app()
-#with app.app_context():
- #   db.create_all() 
-
-#if __name__ == "__main__":
- #   port = int(os.environ.get("PORT", 5000))
-  #  app.run(host="0.0.0.0", port=port)
